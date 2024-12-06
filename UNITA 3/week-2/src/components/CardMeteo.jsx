6d5dfb3261a67bd81bd99417a95a71016d6e03ea
Riganelli Leonardo <leoriganelli1@gmail.com>
2024-12-06 13:44:47 +0100

    aggiornamento

--- UNITA 3/week-2/src/components/CardMeteo.jsx
+++ UNITA 3/week-2/src/components/CardMeteo.jsx
@@ -1,53 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Button, Alert } from "react-bootstrap"
+import { Card, Button } from "react-bootstrap"
 
-const CardMeteo = () => {
-    const key = '3efe250117031385f4bd4b3fc329932b';
-    const [data, setData] = useState({});
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=roma&appid=' + key)
-            .then((response) => {
-                console.log(response); // Aggiunto per il debug
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Il sito non risponde');
-                }
-            })
-            .then((serschResults) => {
-                setData(serschResults);
-                setError(null);
-
-                console.log(data)
-            })
-            .catch((error) => {
-                setError(error.message);
-                console.log('Errore', error);
-            });
-    }, [key]);
-
-    if (error) {
-        return <Alert variant="danger">{error}</Alert>;
-    }
+const CardMeteo = (props) => {
 
     return (
         <>
-            {data && <Card style={{ width: '18rem' }}>
+           <Card style={{ width: '18rem' }}>
                 <div>
-                    <p>{JSON.stringify(data.coord)}</p> {/* Mostra le coordinate in modo leggibile */}
+                    <p>{JSON.stringify(props.coord.lon)}</p> {/* Mostra le coordinate in modo leggibile */}
+                    <p>{JSON.stringify(props.coord.lat)}</p> {/* Mostra le coordinate in modo leggibile */}
                 </div>
                 <Card.Body>
-                    <Card.Title>{data.name}</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                    <Card.Title>{props.name}</Card.Title>
+                    <div>
+                        <p>{props.main.temp_max}</p>
+                        <p>{props.main.temp_min}</p>
+                    </div>
+                    <Button variant="primary">details</Button>
                 </Card.Body>
             </Card>
-            }
         </>
     )
 }
--- UNITA 3/week-2/src/components/MainComponent.jsx
+++ UNITA 3/week-2/src/components/MainComponent.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+import { Alert } from "react-bootstrap";
 import CardMeteo from "./CardMeteo";
 
 const MainComponent = () => {
+
+    const key = '3efe250117031385f4bd4b3fc329932b';
+    const [data, setData] = useState({});
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=roma&appid=' + key)
+            .then((response) => {
+
+                    console.log(response)
+
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Il sito non risponde');
+                }
+            })
+            .then((serschResults) => {
+                setData(serschResults);
+                setError(null);
+            })
+            .catch((error) => {
+                setError(error.message);
+
+                console.log('Errore', error);
+            });
+    }, [key]);
+
+    if (error) {
+        return <Alert variant="danger">{error}</Alert>;
+    }
+
+    console.log(data)
+
     return (
             <>
-                <CardMeteo/>
+                <CardMeteo data = {data}/>
             </>
         )
 }
