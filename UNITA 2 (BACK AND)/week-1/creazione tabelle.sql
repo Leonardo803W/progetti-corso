eb28e8e4b95959b84647f9efc0fbbc2ecf198930
Riganelli Leonardo <leoriganelli1@gmail.com>
2025-02-11 16:33:16 +0100

    BACK-END-UNITA-2-week-2-day-2

--- UNITA 2 (BACK AND)/week-1/creazione tabelle.sql
+++ UNITA 2 (BACK AND)/week-1/creazione tabelle.sql
@@ -1,32 +1,17 @@
-CREATE TABLE edificio (
+CREATE TABLE post (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    indirizzo VARCHAR(100) NOT NULL,
-    citta VARCHAR(100) NOT NULL
+    categoria VARCHAR(50) NOT NULL,
+    titolo VARCHAR(50) NOT NULL,
+    contenuto VARCHAR(50) NOT NULL,
+	tempo_di_lettura INTEGER NOT NULL,
+	autore_id INT,
+	FOREIGN KEY (autore_id) REFERENCES autore(id)
 );
 
-CREATE TABLE postazione (
+CREATE TABLE autore (
     id SERIAL PRIMARY KEY,
-    codice VARCHAR(100) NOT NULL,
-    descrizione VARCHAR(100) NOT NULL,
-    tipo VARCHAR(100) NOT NULL,
-    numero_massimo_occupanti INT NOT NULL,
-    edificio_id INT,
-    FOREIGN KEY (edificio_id) REFERENCES edificio(id)
-);
-
-CREATE TABLE utente (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    nome_completo VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE prenotazione (
-    id SERIAL PRIMARY KEY,
-    postazione_id INT,
-    utente_id INT,
-    data DATE NOT NULL,
-    FOREIGN KEY (postazione_id) REFERENCES postazione(id),
-    FOREIGN KEY (utente_id) REFERENCES utente(id)
+    nome VARCHAR(100) NOT NULL,
+    cognome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    data_di_nascita DATE NOT NULL
 );
